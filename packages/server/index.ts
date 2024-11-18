8e095d84a39de7640e313af82d898bc9f738cb4b
Matt Zeiger <matthew.zeiger47@gmail.com>
2024-11-18 17:55:03 -0500

    fixing build issues

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,30 +1,22 @@
-// import { Server } from "colyseus";
-// import { createServer } from "http";
-// import express from "express";
-// import { GameLobby } from "./room/GameLobby";
-
-
-// const app = express();
-// const httpServer = createServer(app);
-// const gameServer = new Server({ server: httpServer });
-
-// gameServer.define("game_lobby", GameLobby);
-
-// const PORT = 2567;
-// httpServer.listen(PORT, () =>
-//   console.log(`Server running on ws://localhost:${PORT}`),
-// );
-
 import { Server } from "colyseus";
 import { createServer } from "http";
 import express from "express";
+import path from "path";
 import { GameRoom } from "./room/GameRoom";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use Render's dynamic port or default to 3000
 const httpServer = createServer(app);
 const gameServer = new Server({
   server: httpServer,
+});
+
+// Serve React client build files
+app.use(express.static(path.join(__dirname, "../../../client/dist")));
+
+// Catch-all route to serve React's index.html for SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../../client/dist", "index.html"));
 });
 
 gameServer.define("game_room", GameRoom);
@@ -32,6 +24,3 @@ gameServer.define("game_room", GameRoom);
 httpServer.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
-
-
-
